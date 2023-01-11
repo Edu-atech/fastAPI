e5f7d9d765319af42da2e601a4bf9424103278bc
Edu-atech <elucas@atech.com.br>
2023-01-11 20:45:33 -0300

    Update db to sqlite

--- database.py
+++ database.py
@@ -2,9 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql://postgres:postgres@localhost:5434/TodoApplicationDatabase"
-)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
